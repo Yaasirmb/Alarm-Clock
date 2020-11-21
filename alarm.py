4107e4f335085f01c713118930ab84688e02c63e
Yaasir Mahamadou-Bachirou <yaasir.mahamadou@gmail.com>
2020-11-21 14:55:35 -0500

    Script is now a function and plays sound when its the right time

--- alarm.py
+++ alarm.py
@@ -1,24 +1,22 @@
 from datetime import datetime
 import time
+from playsound import playsound
 
 #Program that will act as an alarm clock.
-
 print("Please enter your time in the following format: '07:30:PM \n")
-user = input("When would you like your alarm to ring? ")
-
-when_to_ring = datetime.strptime(user, '%I:%M:%p').time()
-
-
-while True:
+def alarm (user = input("When would you like your alarm to ring? ")):
     
-    time.sleep(2)
-   
+    when_to_ring = datetime.strptime(user, '%I:%M:%p').time()
 
-    current_time = datetime.now().time().strftime("%I:%M:%p")
-    time_to_compare = datetime.now().time().strftime("%I:%M:%p")
+    while True:
+        time.sleep(2)
 
-    #print('Time to compare against: ' + time_to_compare)
-    if when_to_ring.strftime("%I:%M:%p") == current_time:
-        print("ALARM SOUND")
-    
-    print('Current time ' + current_time)
+        current_time = datetime.now().time().strftime("%I:%M:%p")
+        
+        if when_to_ring.strftime("%I:%M:%p") == current_time:
+            playsound('wake_up.mp3')
+
+        
+        print('Current time ' + current_time)
+
+alarm()
